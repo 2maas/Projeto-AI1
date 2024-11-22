43d64d014e04e1a652600772b4d427fa724cbb81
renato934 <98697950+renato934@users.noreply.github.com>
2024-11-22 11:05:12 +0000

    .

--- JS/React.js
+++ JS/React.js
@@ -47,30 +47,4 @@ function App1() {
         filteredComponents.map((component, index) => React.createElement(App,{type: component.type,name: component.name,price: component.price,img: component.img, })));
 }
 
-
-// Mostrar NavBar sem a SideBar
-/*
-function App() {
-    return React.createElement('nav', { className: 'navbar' },
-        React.createElement('div', { className: 'nav-titulo' },
-            React.createElement('a', { href: '../index.html' },
-                React.createElement('img', { src: '../imagens/logo.svg', style: { width: '130px', paddingRight: '5px' } })
-            )
-        ),
-        React.createElement('div', { className: 'nav-conteudo' },
-            React.createElement('ul', { className: 'nav-cont-ul' },
-                React.createElement('li', null,
-                    React.createElement('a', { href: 'MontagemPC.html' }, 'Monte o seu pc')
-                ),
-                React.createElement('li', null,
-                    React.createElement('a', { href: 'SobreNos.html' }, 'Sobre nós')
-                ),
-                React.createElement('li', null,
-                    React.createElement('a', { href: 'ProdutosDisponiveis.html' }, 'Produtos disponíveis')
-                )
-            )
-        )
-    );
-}*/
-
 ReactDOM.render(App1(), document.getElementById('teste'));
