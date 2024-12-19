e52ba0dc412512199c591a38914739f1a4869a53
Tomás <68330651+2maas@users.noreply.github.com>
2024-12-19 22:41:39 +0000

    .

--- JS/Montagem.js
+++ JS/Montagem.js
@@ -29,18 +29,19 @@ function loadComponents() {
 }
 
 function mostrarNaTabela(Componente, tipo) {
-    document.getElementById(tipo + '-img').style.display = 'block';
-    document.getElementById(tipo + '-img').src = Componente.img;
-    imagens.push({nome: Componente.nome, preco:Componente.preco, descricao: Componente.desc ,imagem: Componente.img});
-    document.getElementById(tipo + '-info').textContent = Componente.nome;
-    document.getElementById(tipo + '-preco').textContent = `${Componente.preco.toFixed(2)}€`;
-    document.getElementById(tipo + '-botaoAdd').style.display = 'none';
-    document.getElementById(tipo + '-botaoRemove').style.display = 'inline';
+    if (Componente != '' && tipo != ''){
+        document.getElementById(tipo + '-img').style.display = 'block';
+        document.getElementById(tipo + '-img').src = Componente.img;
+        imagens.push({nome: Componente.nome, preco:Componente.preco, descricao: Componente.desc ,imagem: Componente.img});
+        document.getElementById(tipo + '-info').textContent = Componente.nome;
+        document.getElementById(tipo + '-preco').textContent = `${Componente.preco.toFixed(2)}€`;
+        document.getElementById(tipo + '-botaoAdd').style.display = 'none';
+        document.getElementById(tipo + '-botaoRemove').style.display = 'inline';
 
-    precototal += Componente.preco;
+        precototal += Componente.preco;
 
-    StyleComponentes(tipo);
-
+        StyleComponentes(tipo);
+    }
         
 }
 
